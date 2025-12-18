e472101498742fbd344f2c7c684ea4e28d2f00c6
Youn-sik <yscho20@gmail.com>
2025-12-18 15:08:26 +0900

    [fix] desctination ip 가 항상 0.0.0.0 으로 출력되는 문제 수정

--- ebpf/tcp_connect.c
+++ ebpf/tcp_connect.c
@@ -7,6 +7,17 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+/* 최소 정의만 사용해 커널 헤더 중복을 피한다. */
+#ifndef AF_INET
+#define AF_INET 2
+#endif
+
+struct ipv4_sockaddr {
+    __u16 sin_family;
+    __u16 sin_port;
+    __u32 sin_addr; // network byte order
+};
+
 struct event {
     __u32 daddr; // Destination IPv4 (network byte order) -> convert in Go using binary.BigEndian
 };
@@ -17,15 +28,25 @@ struct {
 } events SEC(".maps");
 
 SEC("kprobe/tcp_v4_connect")
-int BPF_KPROBE(tcp_v4_connect_enter, struct sock *sk)
+int BPF_KPROBE(tcp_v4_connect_enter, struct sock *sk, struct sockaddr *uaddr)
 {
     __u32 daddr;
     struct event *e;
+    struct ipv4_sockaddr sa = {};
 
-    if (!sk)
+    /* 1) 소켓에 기록된 목적지가 있으면 우선 사용한다. */
+    if (sk)
+        daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+
+    /* 2) 비어 있으면 호출자가 넘긴 sockaddr에서 읽어본다. */
+    if (!daddr && uaddr) {
+        bpf_probe_read_kernel(&sa, sizeof(sa), uaddr);
+        if (sa.sin_family == AF_INET) {
+            daddr = sa.sin_addr; // network byte order
+        }
+    }
+    if (!daddr)
         return 0;
-
-    daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
 
     e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
     if (!e)
